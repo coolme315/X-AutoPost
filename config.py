c54d5bc847bad8207a8951cae8de6b68dd5e6382
coolme315 <138187944+coolme315@users.noreply.github.com>
2024-10-15 16:42:20 +0900

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ API_Key_Secret          = 'X API Secret'
 Bearer_Token            = 'X Bearer Token'
 ACCESS_TOKEN            = 'X Access Token'
 ACCESS_TOKEN_SECRET     = 'X Access Secret'
-#{title}=起動時点での配信タイトル　{X}=XのID　{Twitch}=TwitchのURLが入ります。
+#{title}=起動時点での配信タイトル　{X}=XのID　{\n}=改行 {Twitch}=TwitchのURLが入ります。
 Default_Message         = '{title} Twitchで配信開始！ {X} {Twitch}'
