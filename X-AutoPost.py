e033b8c8720fb4605008edbe0f5bbcf3b93c2d93
coolme315 <138187944+coolme315@users.noreply.github.com>
2024-10-15 13:22:45 +0900

    First Release

---
+++ X-AutoPost.py
@@ -0,0 +1,88 @@
+# ライブラリ
+import tweepy
+import requests
+import urllib.parse
+import base64
+import sys, os, asyncio
+import importlib
+
+try:
+    sys.path.append(os.path.dirname(sys.argv[0]))
+    config = importlib.import_module('config')
+except Exception as e:
+    print(e)
+    print('Please make [config.py] and put it with this directory')
+    input() # stop for error!!
+
+# Consumer Keys
+ck = config.API_Key
+cs = config.API_Key_Secret #API KEY SECRETが入ります
+
+# Authentication Tokens
+bt = config.Bearer_Token
+at = config.ACCESS_TOKEN
+ats = config.ACCESS_TOKEN_SECRET
+
+# 認証
+client = tweepy.Client(
+    bearer_token=bt,
+    consumer_key=ck, consumer_secret=cs,
+    access_token=at, access_token_secret=ats
+)
+
+class ChatbotStorage:
+    def __init__(self):
+        self.ready = bool
+        self.channel = None
+        self.threads = [None, None, None, None, None]
+        self.so_args = ["", "", "", "", ""]
+
+    def getUIDfromName(self, name):
+        if name == "":
+            return None
+        r = requests.get('https://api.twitch.tv/helix/users?login=' + name, headers=self.header)
+        if r.status_code == requests.codes.ok:
+            if len(r.json()['data']) > 0:
+                return r.json()['data'][0]['id']
+        return None
+
+    def getChannelInfo(self, cid):
+        if cid == None:
+            return None
+        r = requests.get('https://api.twitch.tv/helix/channels?broadcaster_id=' + cid, headers=self.header)
+        if r.status_code == requests.codes.ok:
+            if len(r.json()['data']) > 0:
+                return r.json()['data'][0]
+        return None
+
+def PostAction():
+    th = {'Authorization': 'OAuth ' + config.OAUTH.replace('oauth:', '') }
+    r = requests.get('https://id.twitch.tv/oauth2/validate', headers=th)
+    dat.header = {
+        'Authorization': 'Bearer '+ config.OAUTH.replace('oauth:', ''),
+        'Client-ID': r.json()['client_id'],
+    }
+
+    uid = dat.getUIDfromName(config.Twitch_Channel)
+    cinfo = dat.getChannelInfo(uid)
+    chn=cinfo['title']
+
+#ツイート内容
+    args_count = len(sys.argv) - 1
+    if not args_count==0:
+        try:
+            posttext=urllib.parse.unquote(base64.b64decode(sys.argv[1]).decode())
+        except:
+            posttext=urllib.parse.unquote(sys.argv[1])
+    else:
+        posttext=urllib.parse.unquote(config.Default_Message.replace('{title}',chn).replace('{X}',config.Twitter_Channel).replace('{Twitch}',"https://twitch.tv/"+config.Twitch_Channel))
+#ツイート
+    client.create_tweet(text=posttext)
+    #print(posttext)
+
+dat = ChatbotStorage() # bot内管理データ:bot internal manage data
+        
+if __name__ == "__main__":
+    ChatbotStorage()
+    PostAction()
+    sys.exit
---
+++ config.py
@@ -0,0 +1,14 @@
+######################################################
+# PLEASE CHANGE FOLLOWING CONFIGS ####################
+Twitch_Channel          = 'TwitchID'
+Twitter_Channel         = '@X ID'
+OAUTH                   = 'TwitchOAuth' # https://twitchapps.com/tmi/
+
+API_Key                 = 'X APIKey'
+API_Key_Secret          = 'X API Secret'
+
+Bearer_Token            = 'X Bearer Token'
+ACCESS_TOKEN            = 'X Access Token'
+ACCESS_TOKEN_SECRET     = 'X Access Secret'
+#{title}=起動時点での配信タイトル　{X}=XのID　{Twitch}=TwitchのURLが入ります。
+Default_Message         = '{title} Twitchで配信開始！ {X} {Twitch}'
